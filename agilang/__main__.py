1495b03ad5b795ddc2f957f39256d9c80d6018eb
Izukanji Silwimba <Tizukanji@gmail.com>
2026-06-22 05:55:28 +0200

    cli: use extended entrypoint

--- agilang/__main__.py
+++ agilang/__main__.py
@@ -1,11 +1,4 @@
-"""
-Entry point for ``python -m agilang``.
-
-This simply forwards to ``agilang.cli.main``.
-"""
-
-from .cli import main
-
+from .cli_runtime import main
 
 if __name__ == "__main__":
     main()
