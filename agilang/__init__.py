f2153ee8c602a71e51ab253934cb2d1214a68dd0
Izukanji Silwimba <Tizukanji@gmail.com>
2026-06-22 08:13:47 +0200

    feat(beacon): export native beacon runtime APIs

--- agilang/__init__.py
+++ agilang/__init__.py
@@ -1,6 +1,6 @@
 """AGILANG production language toolkit."""
 
-__version__ = "2.0.2"
+__version__ = "2.1.0"
 
 from .checker import check_file, check_source
 from .lexer import tokenize
@@ -18,7 +18,7 @@ from .mobile_runtime import mobile_runtime_matrix, mobile_runtime_capabilities, 
 from .lowlevel_network import tcp_listen, tcp_connect, udp_socket, packet_frame, packet_unframe, packet_json, packet_json_parse, gossip_node, lowlevel_network_capabilities
 from .evm import evm_capabilities, evm_keccak, evm_function_selector, evm_abi_encode, evm_abi_decode, evm_contract_call_data, evm_bytecode_builder, evm_disassemble, evm_execute, evm_simulate_call, evm_estimate_gas, evm_trace, evm_world_state, evm_interpreter, evm_rpc, evm_rlp_encode, evm_legacy_unsigned_tx, evm_external_engine
 from .interop import python_package, python_package_status, native_library, capability_manifest, interop_capabilities, systems_capabilities
-from .zk import zk_capabilities, zk_field, zk_circuit, zk_commit, zk_verify_commitment, zk_merkle_tree, zk_merkle_proof, zk_verify_merkle_proof, zk_nullifier, zk_schnorr_keypair, zk_schnorr_prove, zk_schnorr_verify, zk_bridge_status, zk_external_engine, zk_demo_payload
+from .zk import zk_capabilities, zk_field, zk_circuit, zk_commit, zk_verify_commit, zk_merkle_tree, zk_merkle_proof, zk_verify_merkle_proof, zk_nullifier, zk_schnorr_keypair, zk_schnorr_prove, zk_schnorr_verify, zk_bridge_status, zk_external_engine, zk_demo_payload
 
 from .blockchain import blockchain_capabilities, blockchain_config, blockchain_mainnet_config, blockchain_transaction, blockchain_merkle_root, blockchain_node, blockchain_devnet, blockchain_consensus_simulation, blockchain_demo, consensus_engine, pos_consensus_engine, dpos_consensus_engine, dev_consensus_engine, BlockchainNode, BlockchainConfig, ProofOfStakeEngine, DelegatedProofOfStakeEngine, DevConsensusEngine, Mempool, ChainDatabase
 
@@ -29,6 +29,21 @@ except Exception:
     ethereum_consensus_replica_config = None
     ethereum_consensus_check = None
     ethereum_consensus_simulation = None
+
+try:
+    from .beacon import BeaconConfig, BeaconState, BeaconStore, BeaconValidator, beacon_capabilities, create_beacon_state, produce_beacon_block, attest_to_head, process_epoch_finality, fork_choice_head, simulate_beacon
+except Exception:
+    BeaconConfig = None
+    BeaconState = None
+    BeaconStore = None
+    BeaconValidator = None
+    beacon_capabilities = None
+    create_beacon_state = None
+    produce_beacon_block = None
+    attest_to_head = None
+    process_epoch_finality = None
+    fork_choice_head = None
+    simulate_beacon = None
 
 __all__ = [
     "AGILTranslator",
@@ -60,7 +75,6 @@ __all__ = [
     "job_queue",
     "render_ags",
     "seo_tags",
-
     "WebRTCSignalServer",
     "webrtc_signal_server",
     "webrtc_offer",
@@ -85,4 +99,15 @@ __all__ = [
     "run_cgi",
     "run_fastcgi",
     "create_mobile_native_bridge",
+    "BeaconConfig",
+    "BeaconState",
+    "BeaconStore",
+    "BeaconValidator",
+    "beacon_capabilities",
+    "create_beacon_state",
+    "produce_beacon_block",
+    "attest_to_head",
+    "process_epoch_finality",
+    "fork_choice_head",
+    "simulate_beacon",
 ]
